835f493716fc72dfd887ce87f52ac508ab6c2327
Clovis <nachofcz@mail.com>
2020-04-09 18:31:22 -0300

    Basic server working

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const config = require('./config/config');
+var cors = require('cors');
+
 app.listen(process.env.PORT, (err, resp) => {
     console.log(`Server start on port ${process.env.PORT}`)
-})
+});
+
+app.use(cors())
 
 const LanguageController = require('./controller/languageController');
 
@@ -16,3 +21,24 @@ app.get('/js', (req, res) => {
     }
 });
 
+app.get('/', (req, res) => {
+    let html;
+    fs.readFile('../client/index.html', (err, response) => {
+        if (err) {
+            throw new Error(err)
+        }
+        res.write(response);
+        res.end();
+    });
+});
+
+app.get('/index.js', (req, res) => {
+    let html;
+    fs.readFile('../client/index.js', (err, response) => {
+        if (err) {
+            throw new Error(err)
+        }
+        res.write(response);
+        res.end();
+    });
+})
